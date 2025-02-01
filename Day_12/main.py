ee153913a55353ab6622aa553f1acbd32294ea5c
mohamed <maxamedcabdifitaax33@gmail.com>
2025-02-01 12:40:38 +0300

    module

---
+++ Day_12/main.py
@@ -0,0 +1,20 @@
+from mymodule import generate_full_name, sum
+print(generate_full_name('moha', 'mnx'))\
+    
+print(sum(4,9))
+
+from statistics import * # importing all the statistics modules
+ages = [20, 20, 4, 24, 25, 22, 26, 20, 23, 22, 26]
+print(mean(ages))       # ~22.9
+print(median(ages))     # 23
+print(mode(ages))       # 20
+print(stdev(ages))      # ~2.3
+
+
+import math
+print(math.pi)           # 3.141592653589793, pi constant
+print(math.sqrt(2))      # 1.4142135623730951, square root
+print(math.pow(2, 3))    # 8.0, exponential function
+print(math.floor(9.81))  # 9, rounding to the lowest
+print(math.ceil(9.81))   # 10, rounding to the highest
+print(math.log10(100))   # 2, logarithm with 10 as base
---
+++ Day_12/mymodule.py
@@ -0,0 +1,9 @@
+
+
+def generate_full_name(firstname, lastname):
+    return firstname + " " + lastname
+
+def sum (n1, n2): 
+    return n1 + n2
+
+# def object_weight()
